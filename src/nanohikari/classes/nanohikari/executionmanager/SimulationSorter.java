2854b3c59e438bad7b8d1aa307c08fabb4807b9f
Alban Lafuente <alban.lafuente@protonmail.com>
2022-04-25 20:38:03 +0900

    Putting the right package

--- src/nanohikari/classes/nanohikari/executionmanager/SimulationSorter.java
+++ src/nanohikari/classes/nanohikari/executionmanager/SimulationSorter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 alafuente
+ * Copyright (C) 2021 Alban Lafuente
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
